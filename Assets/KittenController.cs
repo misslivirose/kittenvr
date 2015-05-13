38663c0aa943104a202971e9334cfded746c4452
Liv Erickson <livi@outlook.com>
2015-05-13 16:31:00 +0200

    Added script to count kittens, add UI

--- Assets/KittenController.cs
+++ Assets/KittenController.cs
@@ -3,6 +3,8 @@ using System.Collections;
 
 public class KittenController : MonoBehaviour {
 
+	public bool isVisible = false;
+	private float timer = 0.0f;
 	// Use this for initialization
 	void Start () {
 	
@@ -11,17 +13,32 @@ public class KittenController : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		float turnSpeed = 45.0f;
-		if (Input.GetMouseButtonDown(0)) {
+		if (Input.GetMouseButtonDown (0)) {
 			RaycastHit hit;
-			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-			if (Physics.Raycast(ray, out hit))
+			Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+			if (Physics.Raycast (ray, out hit))
 			if (hit.transform != null) {
-				if(hit.transform.gameObject == this.gameObject)
-				{
-					Debug.Log("Hit " + hit.transform.gameObject.name + "at" + this.tag);
-					hit.transform.Rotate(Vector3.up * turnSpeed);
+				if (hit.transform.gameObject == this.gameObject) {
+					Debug.Log ("Hit " + hit.transform.gameObject.name + "at" + this.tag);
+					isVisible = true;
+					hit.transform.Rotate (Vector3.up * turnSpeed);
 				}
 			}
+		}
+		if (isVisible) {
+			timer++;
+		}
+
+		if (timer > 300f) {
+			isVisible = false;
+		}
+	
+	}
+
+	void OnGUI()
+	{
+		if (isVisible) {
+			GUI.Box (new Rect (Screen.width / 2 - 290, Screen.height / 1 - 100, 600, 80), "\nHelp! My kittens have gone missing! \n Can you help me collect all 7 of them?");
 		}
 	}
 }
