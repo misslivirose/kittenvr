6de3d8d7623e7834ecf5c7e8575b2cad100806bf
Liv Erickson <livi@outlook.com>
2015-05-17 10:29:33 +0200

    Updates to KittenController to produce UI based on game status

--- Assets/KittenController.cs
+++ Assets/KittenController.cs
@@ -5,9 +5,11 @@ public class KittenController : MonoBehaviour {
 
 	public bool isVisible = false;
 	private float timer = 0.0f;
+	public GameController playerStats;
+	private string message;
 	// Use this for initialization
 	void Start () {
-	
+
 	}
 	
 	// Update is called once per frame
@@ -19,9 +21,24 @@ public class KittenController : MonoBehaviour {
 			if (Physics.Raycast (ray, out hit))
 			if (hit.transform != null) {
 				if (hit.transform.gameObject == this.gameObject) {
-					Debug.Log ("Hit " + hit.transform.gameObject.name + "at" + this.tag);
-					isVisible = true;
-					hit.transform.Rotate (Vector3.up * turnSpeed);
+
+					if(playerStats.kittens_collected == 0)
+					{
+						message = "Help! My kittens have gone missing! Can you help collect all 7 of them?";
+						isVisible = true;
+					}
+					else if(playerStats.kittens_collected < 7)
+					{
+						message = "You've found " + playerStats.kittens_collected + " kittens so far! Just " + (7 - playerStats.kittens_collected) + " left to go!";
+						isVisible = true;
+					}
+					else
+					{
+						message = "You've found them all! Thank you so meouch!";
+						isVisible = true;
+						System.Threading.Thread.Sleep(2000);
+						Application.LoadLevel(Application.loadedLevel);
+					}
 				}
 			}
 		}
@@ -31,6 +48,7 @@ public class KittenController : MonoBehaviour {
 
 		if (timer > 300f) {
 			isVisible = false;
+			timer = 0f;
 		}
 	
 	}
@@ -38,7 +56,7 @@ public class KittenController : MonoBehaviour {
 	void OnGUI()
 	{
 		if (isVisible) {
-			GUI.Box (new Rect (Screen.width / 2 - 290, Screen.height / 1 - 100, 600, 80), "\nHelp! My kittens have gone missing! \n Can you help me collect all 7 of them?");
+			GUI.Box (new Rect (Screen.width / 2 - 290, Screen.height / 1 - 100, 600, 80), message);
 		}
 	}
 }
