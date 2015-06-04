a7594ba8b77ade2a441e836f3cb3d1f61804c1ab
Livi <livi@outlook.com>
2015-06-03 18:05:31 -0700

    Rotate kitten on y-axis to face user on click

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -75,6 +75,8 @@ public class PlayerController : MonoBehaviour
 			Debug.Log ("You've collected " + kittens_collected + " kittens!");
 		} else if (_VICTIM.tag.Equals ("leader")) {
 			Debug.Log ("Hit Leader");
+			//Call function to orient text facing user
+			OrientTextToUser(_VICTIM);
 			if (kittens_collected == 0) {
 				message = "Help! My kittens have gone missing! Can you help collect all 7 of them?";
 				isVisible = true;
@@ -163,5 +165,19 @@ public class PlayerController : MonoBehaviour
 
 		}
 	}
-
+	/**
+	 * Orient canvas & kitten to face user 
+	 * The canvas is part of the kitten so we just need to orient the gameObject passed in
+	 * Helpful overview link for calculating the rotation: 
+	 * http://bit.ly/1Fvb8am
+	 **/
+	void OrientTextToUser(GameObject baseObject)
+	{
+		float speed = 360f;
+		// Note: Player (this) is the target of the Kitten (baseObject)
+		Vector3 dist = this.transform.position - baseObject.transform.position;
+		Quaternion rot = Quaternion.Slerp (baseObject.transform.rotation, Quaternion.LookRotation (dist), speed * Time.deltaTime);
+		baseObject.transform.rotation = rot;
+		baseObject.transform.eulerAngles = new Vector3 (0, baseObject.transform.eulerAngles.y, 0);	
+	}
 }
