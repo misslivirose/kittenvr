f4ddaf3bcae2ce0e4dfbda1d09aeb7a876039148
Livi <livi@outlook.com>
2016-01-05 20:31:48 -0800

    Adding new level

---
+++ Assets/LoadPage.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEditor.SceneManagement;
+using System.Collections;
+
+public class LoadPage : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(Pause(3.0f));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    IEnumerator Pause(float _seconds)
+    {
+        yield return new WaitForSeconds(_seconds);
+        EditorSceneManager.LoadScene(1);
+
+    }
+}
